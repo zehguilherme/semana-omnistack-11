db134de5ad072076dcd50abc657beccea56c5b35
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-03-25 17:16:09 -0300

    Add new routes: ONG (Create); Incidents (Index, Create and Delete)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,15 +1,17 @@
 const express = require('express')
 
 const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
 
 const routes = express.Router()  //modulo de rotas do express passa para a variavel em questão
 
-// Listar ONGs
-routes.get('/ongs', OngController.index)
+// ONG
+routes.get('/ongs', OngController.index)   // Listar
+routes.post('/ongs', OngController.create) // Criar
 
-/**********************************************************************************************************************/
-
-// Criar ONG
-routes.post('/ongs', OngController.create)
+// Caso
+routes.get('/incidents', IncidentController.index)   // Listar
+routes.post('/incidents', IncidentController.create) // Criar
+routes.delete('/incidents/:id', IncidentController.delete)  // Deletar
 
 module.exports = routes
