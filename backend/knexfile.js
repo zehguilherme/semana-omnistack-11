5f12c5ce2f0e81f3422770bf318ea4ec685626b1
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-03-25 13:29:17 -0300

    Change connection filename and create migrations instantiation with directory inside

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,43 +2,47 @@
 
 module.exports = {
 
-  development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  },
+    development: {
+        client: 'sqlite3',
+        connection: {
+            filename: './src/database/db.sqlite'
+        },
+        migrations: {
+            directory: './src/database/migrations'
+        },
+        useNullAsDefault: true
+    },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+    staging: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
 
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    production: {
+        client: 'postgresql',
+        connection: {
+            database: 'my_db',
+            user: 'username',
+            password: 'password'
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: 'knex_migrations'
+        }
     }
-  }
 
 };
