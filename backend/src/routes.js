7d557365e5cff8503c3185b68d4b79c1acaba948
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-03-25 19:55:11 -0300

    Add new routes: Profile (ProfileController) and Login (SessionController)

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const OngController = require('./controllers/OngController')
 const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
 
 const routes = express.Router()  //modulo de rotas do express passa para a variavel em questão
 
@@ -9,9 +11,15 @@ const routes = express.Router()  //modulo de rotas do express passa para a varia
 routes.get('/ongs', OngController.index)   // Listar
 routes.post('/ongs', OngController.create) // Criar
 
+// Profile
+routes.get('/incidents', ProfileController.index)   // Listar os casos específicos de uma única ONG
+
+// Login - criar (post) uma sessão
+routes.post('/sessions', SessionController.create)
+
 // Caso
-routes.get('/incidents', IncidentController.index)   // Listar
-routes.post('/incidents', IncidentController.create) // Criar
+routes.get('/incidents', IncidentController.index)          // Listar
+routes.post('/incidents', IncidentController.create)        // Criar
 routes.delete('/incidents/:id', IncidentController.delete)  // Deletar
 
 module.exports = routes
