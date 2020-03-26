f79d159a2ca0088effd12db87ede43920312559d
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-03-26 17:12:56 -0300

    Add a counter for the total number of incidents and the display of only 5 of them on each page at a time

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -1,55 +1,66 @@
 const connection = require('../database/connection')  //conexão com o banco
 
 module.exports = {
-    // Listar casos
-    async index (req, res) {
-        const incidents = await connection('incidents').select('*')
+  // Listar casos
+  async index (req, res) {
+    const { page = 1 } = req.query
 
-        return res.json(incidents)
-    },
+    // Retornar a quantidade de casos totais cadastrados
+    const [count] = await connection('incidents').count()
 
-    /*************************************************************************************************************************/
+    const incidents = await connection('incidents')
+      .limit(5)                  // limitar busca para serem mostrados 5 registros por vez
+      .offset((page - 1) * 5)    // pular a cada página de 5 em 5 registros em relação a anterior. Ex: 1 - 1 * 5
+      .select('*')
 
-    // Criar caso (incident)
-    async create (req, res) {
-        const { title, description, value } = req.body
+    //Total de itens na lista
+    res.header('X-Total_Count', count['count(*)'])
 
-        const ong_id = req.headers.authorization // id da ONG logada
+    return res.json(incidents)
+  },
 
-        // Inserir caso
-        const [id] = await connection('incidents').insert({
-            title,
-            description,
-            value,
-            ong_id
-        })
+  /*************************************************************************************************************************/
 
-        return res.json({ id })
-    },
+  // Criar caso (incident)
+  async create (req, res) {
+    const { title, description, value } = req.body
 
-    /*************************************************************************************************************************/
+    const ong_id = req.headers.authorization // id da ONG logada
 
-    // Deletar caso
-    async delete (req, res) {
-        const { id } = req.params
-        const ong_id = req.headers.authorization // id da ONG logada
+    // Inserir caso
+    const [id] = await connection('incidents').insert({
+      title,
+      description,
+      value,
+      ong_id
+    })
 
-        // Busca o caso em que o id é igual ao id vindo do req.params
-        const incident = await connection('incidents')
-            .where('id', id)
-            .select('ong_id')  // selecionar apenas coluna ong_id
-            .first()           // retorna apenas 1 resultado
+    return res.json({ id })
+  },
 
-        // Verifica se o id buscado no banco é diferente do id da ONG logada no momento
-        if (incident.ong_id != ong_id) {
-            return res.status(401).json({ error: 'Operation not permitted.' })
-        }
+  /*************************************************************************************************************************/
 
-        // Deleção do banco de dados
-        await connection('incidents')
-            .where('id', id)
-            .delete()
+  // Deletar caso
+  async delete (req, res) {
+    const { id } = req.params
+    const ong_id = req.headers.authorization // id da ONG logada
 
-        return res.status(204).send()  // 204: resposta com sucesso mas sem conteúdo
+    // Busca o caso em que o id é igual ao id vindo do req.params
+    const incident = await connection('incidents')
+      .where('id', id)
+      .select('ong_id')  // selecionar apenas coluna ong_id
+      .first()           // retorna apenas 1 resultado
+
+    // Verifica se o id buscado no banco é diferente do id da ONG logada no momento
+    if (incident.ong_id != ong_id) {
+      return res.status(401).json({ error: 'Operation not permitted.' })
     }
+
+    // Deleção do banco de dados
+    await connection('incidents')
+      .where('id', id)
+      .delete()
+
+    return res.status(204).send()  // 204: resposta com sucesso mas sem conteúdo
+  }
 }
