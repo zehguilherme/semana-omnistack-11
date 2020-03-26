19998628d49478f4c4feab675f43a95e812794c0
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-03-26 16:59:57 -0300

    Update indentation

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,15 +3,15 @@
 const connection = require('../database/connection')  //conexão com o banco
 
 module.exports = {
-    // Lista os casos específicos de uma única ONG
-    async index (req, res) {
-        const ong_id = req.headers.authorization // id da ONG logada
+  // Lista os casos específicos de uma única ONG
+  async index (req, res) {
+    const ong_id = req.headers.authorization // id da ONG logada
 
-        // Buscar os casos criados pela ong de id ong_id
-        const incidents = await connection('incidents')
-            .where('ong_id', ong_id)
-            .select('*')
+    // Buscar os casos criados pela ong de id ong_id
+    const incidents = await connection('incidents')
+      .where('ong_id', ong_id)
+      .select('*')
 
-        return res.json(incidents)
-    }
+    return res.json(incidents)
+  }
 }
