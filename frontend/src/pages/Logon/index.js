5ec10f2d46eed42c5aac112253c5f330aa098f54
zehguilherme <jgtomaine@hotmail.com>
2020-04-05 18:14:51 -0300

    Add new function handleLogin and update input

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom'
 import { FiLogIn } from 'react-icons/fi'  /*Feather Icons*/
+
+import api from '../../services/api'
 
 import './styles.css'
 
@@ -8,14 +10,43 @@ import logoImg from '../../assets/logo.svg'
 import herosImg from '../../assets/heroes.png'
 
 export default function Logon () {
+  const [id, setId] = useState('')
+
+  const history = useHistory()
+
+  // Responsável pelo login do usuário - disparada quando o form der 'submit'
+  async function handleLogin (e) {
+    e.preventDefault()  //vai previnir o comportamento padrão do form (recarregar página toda)
+
+    try {
+      const response = await api.post('/sessions', { id })  //enviar o id da ONG que está querendo logar
+
+      // Salvar localmente no navegador
+      localStorage.setItem('ongId', id)
+      localStorage.setItem('ongName', response.data.name)
+
+      // Enviar usuário para página de listagem de casos (Profile)
+      history.push('/profile')
+
+    } catch (error) {
+      alert('Falha no login, tente novamente.')
+    }
+  }
+
   return (
     <div className="logon-container">
       <section className="form">
         <img src={logoImg} alt="Be The Hero" />
-        <form action="">
+
+        <form onSubmit={handleLogin}>
           <h1>Faça seu logon</h1>
 
-          <input placeholder="Sua ID" />
+          <input
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+          />
+
           <button className="button" type="submit">Entrar</button>
 
           <Link className="back-link" to="/register">
