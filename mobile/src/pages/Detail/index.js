c3c64f85f451a0ff04a8b131b2ec9b83e5a1a9aa
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-04-22 19:10:02 -0300

    Add useRoute; change message, subject, recipients; functions sendWhatsapp and Text components

--- mobile/src/pages/Detail/index.js
+++ mobile/src/pages/Detail/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Feather } from '@expo/vector-icons'
-import { useNavigation } from '@react-navigation/native'
+import { useNavigation, useRoute } from '@react-navigation/native'
 import { Image, Text, View, TouchableOpacity, Linking } from 'react-native'
 import * as MailComposer from 'expo-mail-composer'
 
@@ -10,7 +10,10 @@ import styles from './styles'
 
 export default function Detail () {
   const navigation = useNavigation()
-  const message = 'Olá APAD, estou entrando em contato pois gostaria de ajudar no caso "Cadelinha atropelada" com o valor de R$ 120,00'
+  const route = useRoute()  //pega informações específicas da página atual da aplicação
+
+  const incident = route.params.incident
+  const message = `Olá ${incident.name}, estou entrando em contato pois gostaria de ajudar no caso "${incident.title}" com o valor de ${Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(incident.value)}`
 
   // Navegar usuário de volta para a página de casos
   function navigateBack () {
@@ -20,15 +23,15 @@ export default function Detail () {
   // Enviar e-mail através do clique no botão
   function sendMail () {
     MailComposer.composeAsync({
-      subject: 'Herói do caso: Cadelinha atropelada', //assunto da mensagem
-      recipients: ['jgtomaine@hotmail.com'], //para quem o e-mail será enviado
+      subject: `Herói do caso: ${incident.title}`, //assunto da mensagem
+      recipients: [incident.email], //para quem o e-mail será enviado
       body: message //conteúdo da mensagem
     })
   }
 
   // Enviar whatsapp
   function sendWhatsapp () {
-    Linking.openURL(`whatsapp://send?phone=+5514981195569&text=${message}`)
+    Linking.openURL(`whatsapp://send?phone=${incident.whatsapp}&text=${message}`)
   }
 
   return (
@@ -43,13 +46,18 @@ export default function Detail () {
 
       <View style={styles.incident}>
         <Text style={[styles.incidentProperty, { marginTop: 0 }]}>ONG:</Text>
-        <Text style={styles.incidentValue}>APAD</Text>
+        <Text style={styles.incidentValue}>{incident.name} de {incident.city}/{incident.uf}</Text>
 
         <Text style={styles.incidentProperty}>CASO:</Text>
-        <Text style={styles.incidentValue}>Cadelinha atropelada.</Text>
+        <Text style={styles.incidentValue}>{incident.title}.</Text>
 
         <Text style={styles.incidentProperty}>VALOR:</Text>
-        <Text style={styles.incidentValue}>R$ 120,00</Text>
+        <Text style={styles.incidentValue}>
+          {Intl.NumberFormat('pt-BR', {
+            style: 'currency',
+            currency: 'BRL'
+          }).format(incident.value)}
+        </Text>
       </View>
 
       <View style={styles.contactBox}>
