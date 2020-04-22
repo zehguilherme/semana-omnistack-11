244adeca90c4d34d166a7169809bd70ef1539673
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-04-22 19:14:25 -0300

    Add new useState: page, loading; modify navigateToDetail(), loadIncidents(); FlatList and TouchableOpacity components

--- mobile/src/pages/Incidents/index.js
+++ mobile/src/pages/Incidents/index.js
@@ -13,20 +13,41 @@ export default function Incidents () {
   const [incidents, setIncidents] = useState([])  //começa com array vazio
   const [total, setTotal] = useState(0)  //estado para armazenar o total de itens
 
+  const [page, setPage] = useState(1) //controla o nº da página em que se está no momento - inicia sempre na página 1
+  const [loading, setLoading] = useState(false) //armazenar uma informação quando se está buscando dados novos para evitar que eles sejam buscados novamente
 
   const navigation = useNavigation()  //permite a navegação do usuário
 
   // Navegar usuário para a página de detalhes de cada caso
-  function navigateToDetail () {
-    navigation.navigate('Detail')
+  function navigateToDetail (incident) {
+    navigation.navigate('Detail', { incident })
   }
 
   // Carregar casos
   async function loadIncidents () {
-    const response = await api.get('/incidents')
+    // evitar com que enquanto outra requisição seja feita, com que mais uma requisição venha a acontecer
+    if (loading) {
+      return
+    }
 
-    setIncidents(response.data)
+    // Se o total de casos já estiver carregado do banco (carregou pelo menos a 1ª página)
+    // && nº incidents da lista for igual ao total
+    if (total > 0 && incidents.length === total) {
+      return // Não irá buscar mais informações
+    }
+
+    setLoading(true)
+
+    const response = await api.get('incidents', {
+      params: { page }
+    })
+
+    setIncidents([...incidents, ...response.data])  //anexar 2 vetores dentro de apenas 1 (todos os valores que já tem dentro dos casos + todos que vem de response.data)
+
     setTotal(response.headers['x-total-count'])  //total de casos
+
+    setPage(page + 1)  //pula para a próxima página
+    setLoading(false)
   }
 
   useEffect(() => {
@@ -49,8 +70,10 @@ export default function Incidents () {
         data={incidents}                       //vetor de dados que monta a lista de itens
         style={styles.incidentList}
         keyExtractor={incident => String(incident.id)}  //usa o id de cada indicent para determinar qual é qual (informação única para cada 1 deles)
-        showsVerticalScrollIndicator={false}         //tira a barra que indica o scroll da página
-        renderItem={({ item: incident }) => (                          //renderItem: função responsável que armazena cada item da lista
+        showsVerticalScrollIndicator={false}            //tira a barra que indica o scroll da página
+        onEndReached={loadIncidents}                    //dispara uma função quando usuário chega no final da lista
+        onEndReachedThreshold={0.2}                     //quantos % do final da lista o usuário precisa estar para que sejam carregados novo itens (0 a 1)
+        renderItem={({ item: incident }) => (           //renderItem: função responsável que armazena cada item da lista
           <View style={styles.incident}>
             <Text style={styles.incidentProperty}>ONG:</Text>
             <Text style={styles.incidentValue}>{incident.name}</Text>
@@ -68,7 +91,7 @@ export default function Incidents () {
 
             <TouchableOpacity
               style={styles.detailsButton}
-              onPress={navigateToDetail}
+              onPress={() => navigateToDetail(incident)}
             >
 
               <Text style={styles.detailsButtonText}>Ver mais detalhes</Text>
