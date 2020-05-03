c91ee8e8287b12574cd17295be3ff9a6672e8299
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-05-03 17:26:48 -0300

    Creation of ternary condition for test configurations and development database

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,6 +1,10 @@
+/* eslint-disable no-undef */
 const knex = require('knex')
 const configuration = require('../../knexfile')  //configurações do banco de dados
 
-const connection = knex(configuration.development)
+//variáveis ambientes
+const config = process.env.NODE_ENV === 'test' ? configuration.test : configuration.development
+
+const connection = knex(config)
 
 module.exports = connection
