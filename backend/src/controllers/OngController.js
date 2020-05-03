4a06452e5c9c70e1b5b95cc0c0bbb84a11d3b4c0
zehguilherme <36301054+zehguilherme@users.noreply.github.com>
2020-05-03 17:31:18 -0300

    Change in the receipt of id generation and instantiation of the constant used

--- backend/src/controllers/OngController.js
+++ backend/src/controllers/OngController.js
@@ -1,34 +1,34 @@
-const crypto = require('crypto')
+const generateUniqueId = require('../utils/generateUniqueId')
 const connection = require('../database/connection')  //conexão com o banco
 
 module.exports = {
-    // Listar ONGs
-    async index (req, res) {
-        const ongs = await connection('ongs').select('*')  //todos os registros
+  // Listar ONGs
+  async index (req, res) {
+    const ongs = await connection('ongs').select('*')  //todos os registros
 
-        return res.json(ongs)
-    },
+    return res.json(ongs)
+  },
 
-    /*******************************************************************************************************************************/
+  /*******************************************************************************************************************************/
 
-    // Criação de ONG
-    async create (req, res) {
-        const { name, email, whatsapp, city, uf } = req.body
+  // Criação de ONG
+  async create (req, res) {
+    const { name, email, whatsapp, city, uf } = req.body
 
-        const id = crypto.randomBytes(4).toString('HEX')  //geração de 4 bites de caracteres hexadecimais (letras)
+    const id = generateUniqueId()
 
-        // Inserir ONG
-        await connection('ongs').insert({
-            id,
-            name,
-            email,
-            whatsapp,
-            city,
-            uf
-        })
+    // Inserir ONG
+    await connection('ongs').insert({
+      id,
+      name,
+      email,
+      whatsapp,
+      city,
+      uf
+    })
 
-        return res.json({ id })
-    }
+    return res.json({ id })
+  }
 }
 
 /*******************************************************************************************************************************/
